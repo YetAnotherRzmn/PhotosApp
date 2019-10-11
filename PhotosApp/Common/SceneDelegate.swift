649fca77b45a1fe0ca9d2f8f86277c7c3934e6c6
YetAnotherRzmn <nprzmn@gmail.com>
2019-10-11 18:13:58 +0300

    created xcodeproj

---
+++ PhotosApp/Common/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  PhotosApp
+//
+//  Created by Nikita Razumnuy on 10/11/19.
+//  Copyright © 2019 Nikita Razumnuy. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+			   willConnectTo session: UISceneSession,
+			   options connectionOptions: UIScene.ConnectionOptions) {
+    	guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
